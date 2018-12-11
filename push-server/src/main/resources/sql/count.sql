9b01d9f943460f4e16301b3350b0a5f34b91a582
jellyb <jelly_b@126.com>
2018-12-11 17:21:26 +0800

    query sql

---
+++ push-server/src/main/resources/sql/count.sql
@@ -0,0 +1,17 @@
+select t0.num0,t1.num1,t2.num2,t3.num3,t4.num4,t5.num5,t6.num6,t7.num7,t8.num8,t9.num9,t10.num10,t11.num11,t12.num12,t13.num13,t14.num14,t15.num15 from
+(select count(*) as num0 from r_notice_user_00) t0,
+(select count(*) as num1 from r_notice_user_01) t1,
+(select count(*) as num2 from r_notice_user_02) t2,
+(select count(*) as num3 from r_notice_user_03) t3,
+(select count(*) as num4 from r_notice_user_04) t4,
+(select count(*) as num5 from r_notice_user_05) t5,
+(select count(*) as num6 from r_notice_user_06) t6,
+(select count(*) as num7 from r_notice_user_07) t7,
+(select count(*) as num8 from r_notice_user_08) t8,
+(select count(*) as num9 from r_notice_user_09) t9,
+(select count(*) as num10 from r_notice_user_10) t10,
+(select count(*) as num11 from r_notice_user_11) t11,
+(select count(*) as num12 from r_notice_user_12) t12,
+(select count(*) as num13 from r_notice_user_13) t13,
+(select count(*) as num14 from r_notice_user_14) t14,
+(select count(*) as num15 from r_notice_user_15) t15
