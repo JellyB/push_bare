4e3b0f8fab0ccc82dc7a41294ccc9727a3d1dbff
jellyb <jelly_b@126.com>
2018-12-26 16:18:33 +0800

    add cors

--- push-server/src/main/java/com/huatu/tiku/push/spring/conf/WebConfig.java
+++ push-server/src/main/java/com/huatu/tiku/push/spring/conf/WebConfig.java
@@ -3,6 +3,8 @@ package com.huatu.tiku.push.spring.conf;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * 描述：
@@ -11,10 +13,15 @@ import org.springframework.web.client.RestTemplate;
  * Create time 2018-12-07 上午10:04
  **/
 @Configuration
-public class WebConfig {
+public class WebConfig extends WebMvcConfigurerAdapter{
 
     @Bean
     public RestTemplate restTemplate(){
         return new RestTemplate();
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/push/pc/**");
     }
 }
