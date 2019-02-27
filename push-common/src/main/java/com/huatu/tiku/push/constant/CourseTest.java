65742bb7d06acf3a465c02660e712da74bf5b6b0
jellyb <jelly_b@126.com>
2019-02-27 10:36:14 +0800

    阶段测试

---
+++ push-common/src/main/java/com/huatu/tiku/push/constant/CourseTest.java
@@ -0,0 +1,11 @@
+package com.huatu.tiku.push.constant;
+
+/**
+ * 描述：
+ *
+ * @author biguodong
+ * Create time 2019-02-27 上午10:33
+ **/
+public abstract class CourseTest extends Params {
+    public static final String TYPE = "courseTest";
+}
