04d6b33cfa6aa19a09da7cee530c3581065efc5f
jellyb <jelly_b@126.com>
2019-02-27 10:20:52 +0800

    notice View

---
+++ push-common/src/main/java/com/huatu/tiku/push/constant/NoticeViewConstant.java
@@ -0,0 +1,18 @@
+package com.huatu.tiku.push.constant;
+
+/**
+ * 描述：我的消息视图列表
+ *
+ * @author biguodong
+ * Create time 2019-02-27 上午10:11
+ **/
+public class NoticeViewConstant {
+
+    public static final String COURSE = "course";
+
+    public static final String PLAT_FORM = "platForm";
+
+    public static final String LOGISTICS = "logistics";
+
+    public static final String FEED_BACK = "feedBack";
+}
